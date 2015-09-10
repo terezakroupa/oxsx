e440e15c5fec26aeb361f7ee2c22255cf8107297
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-09-10 16:30:56 +0100

    ..and the header

---
+++ src/data/Handle.h
@@ -0,0 +1,21 @@
+/**************************************************************/
+/* ABC for a data set from any origin, hands out data to oxsx */
+/**************************************************************/
+#ifndef __HANDLE__
+#define __HANDLE__
+#include <stddef.h>
+
+class DataHandler;
+class Handle {
+ public:
+    virtual DataHandler GetEntry(size_t iEvent_) = 0;
+    unsigned GetNEntries() const {return fNEntries;}
+    
+ private:
+    unsigned fNVar;
+    unsigned fNEntries;
+    
+    DataHandler Assemble(size_t iEvent_) const;
+};
+
+#endif
