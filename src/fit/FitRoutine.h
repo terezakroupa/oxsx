fb904f29f45a130ea5bdb6f7ce847de7f3065c0a
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-09-12 17:04:30 +0100

    src/fit

--- src/fit/FitRoutine.h
+++ src/fit/FitRoutine.h
@@ -24,11 +24,17 @@ class FitRoutine{
     FitResult Fit();
     
  private:
-    Handle*            fHandle;
-    Optimiser*         fOptimiser;
-    Evaluator*         fEvaluator;
-    PdfCollection      fPdfs;
-    SystematicManager  fSystematicManager;
-    
+    Handle*            fHandle;    // responsible for data
+    Optimiser*         fOptimiser; // responsible for optimisation
+    Evaluator*         fEvaluator; // evaluates test stat from data and pdf
+    PdfCollection      fPdfs;      // Gives probabilities of observations
+    SystematicManager  fSystematicManager; // responsible for the systematics
+
+    size_t fNPdfs;
+    size_t fNSystematics;
+    std::vector<double> fSystematicParams;
+    std::vector<double> fNormalisationParams; 
+    void SplitParams(const std::vector<double>& params_); //into systamatics and normalisations
+       
 };
 #endif
