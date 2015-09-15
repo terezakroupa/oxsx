f9318c15599f1c7dff5be86f4f434605a187d19e
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2015-09-15 18:25:14 +0100

    extended likelihood

--- src/teststat/NLLH.cpp
+++ src/teststat/NLLH.cpp
@@ -3,19 +3,28 @@
 #include <Handle.h>
 double 
 NLLH::Evaluate(){
-
     // Adjust Systematics
     fSystematicManager.SetParameters(fSystematicParams);
     
     // Apply systematics
     fPdfManager.ApplySystematics(fSystematicManager.GetSystematics());
 
+    // Set Normalisations
+    fPdfManager.SetNormalisations(fNormalisations);
+
     // loop over events and calculate P(data | {N, #delta})
-    double log_prob = 0;
+    double nLogLH = 0;
     for(size_t i = 0; i < fHandle -> GetNEntries(); i++){
         DataHandler eventData = fHandle->GetEntry(i);
-        log_prob -= fPdfManager.Probability(eventData);
+        nLogLH -= fPdfManager.Probability(eventData);
     }
 
-    return log_prob;
+    // Extended LH correction
+    for(size_t i = 0; i < fNormalisations.size(); i++)
+        nLogLH += fNormalisations.at(i);
+
+    // Constraints
+    
+    
+    return nLogLH;
 }
