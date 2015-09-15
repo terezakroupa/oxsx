d790eb45951c11a4faacba7779495d1d341709ea
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2015-09-15 16:59:19 +0100

    Restructure Optimisation Port in MINUIT

--- src/optimise/Optimiser.h
+++ src/optimise/Optimiser.h
@@ -4,25 +4,18 @@
 #ifndef __OPTIMISER__
 #define __OPTIMISER__
 #include <vector>
+#include <Evaluator.h>
 
-class Evaluator;
 class FitResult;
 
 class Optimiser{
  public:
-    Optimiser(Evaluator* ev_) : pEvaluator(ev_) {}
+    Optimiser(Evaluator* evaluator_): pEvaluator(evaluator_){}
     virtual ~Optimiser();
-    
-    virtual bool Iterate() = 0;
-    FitResult GetFitResult();
-    
-    const std::vector<double>& GetParams() const;
-    void  SetTestValue(double);
 
- private:
-    Evaluator* pEvaluator; 
-    bool fHasConverged;
-    std::vector<double> fParams;
-    double fTestValue;
+    virtual void Optimise() = 0;
+
+ protected:
+    Evaluator* pEvaluator;
 };
 #endif
