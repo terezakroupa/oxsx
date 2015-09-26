e6db38cb808ab7ab87c411f614899a46467359f1
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-09-26 19:04:07 +0100

    style changes

--- src/pdf/binned/BinnedPdf.cpp
+++ src/pdf/binned/BinnedPdf.cpp
@@ -13,7 +13,8 @@ BinnedPdf::BinnedPdf(const BinnedPdf& other_) : Pdf(other_){
     fNBins = fBinContents.size();
 }
 
-void BinnedPdf::SetAxes(const AxisCollection& axes_){
+void 
+BinnedPdf::SetAxes(const AxisCollection& axes_){
     fAxes  = axes_;
     fNBins = fAxes.GetNBins();
     fNDims = fAxes.GetNDimensions();
@@ -21,73 +22,89 @@ void BinnedPdf::SetAxes(const AxisCollection& axes_){
     
 }
 
-const AxisCollection& BinnedPdf::GetAxes() const{
+const AxisCollection& 
+BinnedPdf::GetAxes() const{
     return fAxes;
 }
 
-double BinnedPdf::operator() (const std::vector<double>& vals_) const{
+double 
+BinnedPdf::operator() (const std::vector<double>& vals_) const{
     return fBinContents.at(FindBin(vals_));
 }
 
-double BinnedPdf::Integral() const{
+double 
+BinnedPdf::Integral() const{
     double sum = 0;
     for(size_t i = 0; i < fNBins; i++)
         sum += fBinContents[i];
     return sum;
 }
 
-void BinnedPdf::Normalise(){
+void 
+BinnedPdf::Normalise(){
     double sum = Integral();
     for(size_t i = 0; i < fNBins; i++)
         fBinContents[i] /= sum;
 }
 
-Pdf* BinnedPdf::Clone() const{
+Pdf* 
+BinnedPdf::Clone() const{
     return static_cast<Pdf*>(new BinnedPdf(*this));
 }
 
-void BinnedPdf::Fill(const std::vector<double>& vals_, double weight_){
+void 
+BinnedPdf::Fill(const std::vector<double>& vals_, double weight_){
     fBinContents[FindBin(vals_)] += weight_;
 }
 
-void BinnedPdf::Fill(const EventData& data_, double weight_){
+void 
+BinnedPdf::Fill(const EventData& data_, double weight_){
     Fill(data_.ToRepresentation(fDataRep), weight_);
 }
 
-size_t BinnedPdf::FindBin(const std::vector<double>& vals_) const{
+size_t 
+BinnedPdf::FindBin(const std::vector<double>& vals_) const{
     return fAxes.FindBin(vals_);
     
 }
 
-size_t BinnedPdf::FindBin(const EventData& data_) const{
+size_t 
+BinnedPdf::FindBin(const EventData& data_) const{
     return fAxes.FindBin(data_.ToRepresentation(fDataRep));    
 }
 
-double BinnedPdf::GetBinContent(size_t bin_) const{
+double 
+BinnedPdf::GetBinContent(size_t bin_) const{
     if(bin_ > fNBins)
         throw OutOfBoundsError("Out of bounds bin access attempted!");
     return fBinContents[bin_];
 }
 
-void BinnedPdf::AddBinContent(size_t bin_, double content_){
+void 
+BinnedPdf::AddBinContent(size_t bin_, double content_){
     if(bin_ > fNBins)
         throw 0; //FIXME should be outofbounds error
     fBinContents[bin_] += content_;
 }
 
-size_t BinnedPdf::GetNBins() const{
+size_t 
+BinnedPdf::GetNBins() const{
     return fNBins;
 }
 
-void BinnedPdf::Empty(){
+void 
+BinnedPdf::Empty(){
     for(size_t i = 0; i < fNBins; i++)
         fBinContents[i] = 0;
 }
 
-size_t BinnedPdf::FlattenIndicies(const std::vector<size_t>& indicies_) const{
+size_t 
+BinnedPdf::FlattenIndicies(const std::vector<size_t>& indicies_) const{
     return fAxes.FlattenIndicies(indicies_);
 }
 
-std::vector<size_t> BinnedPdf::UnpackIndicies(size_t bin_) const{
+std::vector<size_t> 
+BinnedPdf::UnpackIndicies(size_t bin_) const{
     return fAxes.UnpackIndicies(bin_);
 }
+
