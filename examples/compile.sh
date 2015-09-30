9e90555ab4ea6cfa2920e62e1def68560e8c3c1a
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2015-09-30 18:11:59 +0100

    start examples

---
+++ examples/compile.sh
@@ -0,0 +1,12 @@
+str=""
+for dir in $(find $OXSXROOT/src -type d)
+do
+str="-I $dir $str"
+done
+
+
+
+outname=$(basename $1)
+outname="${outname%.*}"
+
+g++ -L$OXSXROOT/build -L/opt/local/lib -I/opt/local/include -loxsx -lMinuit2 -larmadillo -lgsl -o $outname $str `root-config --cflags --glibs`  $1
