ab541a400d4b3aa7aa5592bf606530b33269d574
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-10-01 22:58:56 +0100

    lib paths

--- env.sh
+++ env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 OXSXROOT=/Users/Jack/snoplus/oxsx
-PATH=/Users/Jack/snoplus/snoing/install/root-5.34.30/bin:/Users/Jack/snoplus/snoing/install/scons-2.3.4/script:$PATH
-PYTHONPATH=$OXSXROOT/util:/Users/Jack/snoplus/snoing/install/scons-2.3.4/engine:$PYTHONPATH
-LD_LIBRARY_PATH=$OXSXROOT/build:$LD_LIBRARY_PATH
 
+PATH=/Users/Jack/snoplus/snoing/install/root-5.34.30/bin:/Users/Jack/snoplus/snoing/install/scons-2.3.4/script:/Users/jack/snoplus/snoing/install/root-5.34.30/include$PATH
+
+PYTHONPATH=$OXSXROOT/util:/Users/Jack/snoplus/snoing/install/scons-2.3.4/engine:$PYTHONPATH
+LD_LIBRARY_PATH=$OXSXROOT/build:/Users/Jack/snoplus/snoing/install/root-5.34.30/lib:$LD_LIBRARY_PATH
+
+DYLD_LIBRARY_PATH=/Users/Jack/snoplus/snoing/install/root-5.34.30/lib:$DYLD_LIBRARY_PATH
 export ROOTSYS=/Users/Jack/snoplus/snoing/install/root-5.34.30
 export OXSX_SCONS=/Users/Jack/snoplus/snoing/install/scons-2.3.4
-export LD_LIBRARY_PATH PYTHONPATH OXSXROOT
+export LD_LIBRARY_PATH PYTHONPATH OXSXROOT DYLD_LIBRARY_PATH
