21d7ced1b22963362be14980fc9af8ea6fda07a2
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2015-10-07 12:15:13 +0100

    update env.sh

--- env.sh
+++ env.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #Set these for the installation paths
-SCONS_DIR=/data/snoplus/software/snocave_SL6/scons-2.3.4/
-ROOT_DIR=/data/snoplus/software/snocave_SL6/root-5.34.30/
+SCONS_DIR=/Users/Jack/snoplus/snoing/install/scons-2.3.4
+ROOT_DIR=/Users/Jack/snoplus/snoing/install/root-5.34.30
 
 # location of armadillo includes and libraries. 
 # If these are installed to any of the standard /usr/... they may be left blank
 ARMA_LIB=""
 ARMA_HEADER=""
-GSL_LIB=""
-GSL_HEADER=""
+GSL_LIB=/opt/local/lib
+GSL_HEADER=/opt/local/include
 
 #####################################################
 # Shouldn't need to change anything below this line #
@@ -21,5 +21,7 @@ PATH=$ROOT_DIR/bin:/$SCONS_DIR/script:$PATH
 
 #tells python where the utility functions are
 PYTHONPATH=$OXSXROOT/util:/data/snoplus/software/snocave_SL6/scons-2.3.4/engine:$PYTHONPATH
-LD_LIBRARY_PATH=$ROOT_DIR/lib:$LD_LIBRARY_PATH
-export ARMA_HEADER ARMA_LIB GSL_LIB GSL_HEADER PYTHONPATH LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$OXSXROOT/build:$ROOT_DIR/lib:$LD_LIBRARY_PATH
+DYLD_LIBRARY_PATH=$ROOT_DIR/lib:$ARMA_LIB:$DYLD_LIBRARY_PATH
+
+export ARMA_HEADER ARMA_LIB GSL_LIB GSL_HEADER PYTHONPATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH
