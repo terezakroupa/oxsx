ec5fb5c03ab5812080f5294694ddba4b944336ca
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2015-11-07 16:17:19 +0000

    update header guard error

--- src/interval/BayesIntervalCalc.h
+++ src/interval/BayesIntervalCalc.h
@@ -1,10 +1,10 @@
-#ifndef __OXSX_HISTOGRAM__
-#define __OXSX_HISTOGRAM__
+#ifndef __OXSX_BAYESINTERVALCALC__
+#define __OXSX_BAYESINTERVALCALC__
 #include <Histogram.h>
 
 class BayesIntervalCalc{
  public:
-    static double LHLowerBound(const Histogram& lh_, double cl_); // needs to be a 1D histogram
+    static double UpperBound(Histogram lh_, double cl_); // needs to be a 1D histogram
     
 };
 #endif
