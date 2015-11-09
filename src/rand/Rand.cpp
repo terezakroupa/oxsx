bc5e1b4fcbe17833ff2ddfba7c2beeb7ee0b084b
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2015-11-09 10:46:25 +0000

    root rand nums, set seed

--- src/rand/Rand.cpp
+++ src/rand/Rand.cpp
@@ -8,11 +8,16 @@ Rand::fRandomGen = TRandom3();
 
 double
 Rand::Uniform(double max_){
-    return max_ * ((double) rand() / (RAND_MAX));
+    return max_ * fRandomGen.Rndm();
 }
 
 double 
 Rand::Gaus(double mean_, double sigma_){
     //    rand3.SetSeed();
     return fRandomGen.Gaus(mean_, sigma_);
+}
+
+void
+Rand::SetSeed(unsigned seed_){
+    fRandomGen.SetSeed(seed_);
 }
