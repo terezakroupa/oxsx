5b97b685801d6402be23316d0655507b10101e93
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2016-03-20 00:33:09 -0400

    pdf filler works with no cuts

--- examples/Cuts.cpp
+++ examples/Cuts.cpp
@@ -6,9 +6,52 @@ The CutCollection object is used to FillPdfs or generate data sets, only taking 
 */
 #include <BoxCut.h>
 #include <BoolCut.h>
+#include <LineCut.h>
+#include <CutCollection.h>
 #include <EventData.h>
+#include <iostream>
 
 int main(){
-    // cut if first observable is bigger than zero
-    BoxCut
+    // cut if first observable is bigger than two i.e. a lower limit
+    LineCut lineCut(0, 2, "lower");
+    
+    // cut if second observable is between 5 and 10
+    BoxCut boxCut(1, 5, 10);
+
+    // cut if the third value is 1 exactly
+    BoolCut boolCut(2, 1);
+
+    // Combine the three cuts
+    CutCollection combinedCuts;
+    combinedCuts.AddCut(lineCut);
+    combinedCuts.AddCut(boxCut);
+    combinedCuts.AddCut(boolCut);
+
+    // Create a fake event to test
+    std::vector<double> observations;
+    observations.push_back(1);
+    observations.push_back(6);
+    observations.push_back(1);
+
+    EventData fakeEvent(observations);
+
+    // test it
+    std::cout << "Event passes line cut " 
+              << lineCut.PassesCut(fakeEvent)
+              << std::endl;
+
+    std::cout << "Event passes box cut " 
+              << boxCut.PassesCut(fakeEvent)
+              << std::endl;
+
+    std::cout << "Event passes bool cut " 
+              << boolCut.PassesCut(fakeEvent)
+              << std::endl;
+
+    std::cout << "Event passes all cuts " 
+              << combinedCuts.PassesCuts(fakeEvent)
+              << std::endl;
+
+    return 0;
+    
 }
