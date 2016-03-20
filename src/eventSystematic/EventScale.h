b977b331c10d49e7121c702539e1964a719beef5
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2016-03-20 19:04:02 -0400

    event scaler

---
+++ src/eventSystematic/EventScale.h
@@ -0,0 +1,19 @@
+#ifndef __OXSX_EVENT_SCALE__
+#define __OXSX_EVENT_SCALE__
+#include <EventSystematic.h>
+#include <vector>
+
+class EventScale : public EventSystematic{
+ public:
+    virtual EventData operator()(const EventData&);
+
+    virtual void   SetParameters(const std::vector<double>&);
+    virtual void   SetParameter(size_t index_, double val_);
+    virtual double GetParameter(size_t index_) const;
+
+    void   SetScale(double);
+    double GetScale() const;
+    
+    virtual void SetDataRep(const DataRepresentation&);
+};
+#endif
