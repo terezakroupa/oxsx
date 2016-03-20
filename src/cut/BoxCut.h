045ed90d988c3e785322c38362ea3f9de2a10c53
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-03-20 00:30:08 -0400

    cut updates

--- src/cut/BoxCut.h
+++ src/cut/BoxCut.h
@@ -8,7 +8,6 @@ class BoxCut : public Cut{
     BoxCut(size_t dim_, double lower_, double upper_) : fDim(dim_), fUpperLim(upper_), 
                                                         fLowerLim(lower_) {}
 
-    BoxCut(const BoxCut& other_);
     virtual ~BoxCut(){}
     virtual Cut* Clone() const;
 
