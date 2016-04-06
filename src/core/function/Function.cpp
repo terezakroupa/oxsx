093fb3784800248808c4c654fdf74c61b96f0e86
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-04-06 21:12:34 +0100

    move functionality from integrable pdf to function

--- src/core/function/Function.cpp
+++ src/core/function/Function.cpp
@@ -1,5 +1,5 @@
 #include <Function.h>
-
+#include <PdfExceptions.h>
 void
 Function::SetNDims(unsigned nDims_){
     fNDims = nDims_;    
@@ -8,5 +8,38 @@ Function::SetNDims(unsigned nDims_){
 unsigned 
 Function::GetNDims() const{
     return fNDims;
+}
+
+double
+Function::Call1D(double val_) const{
+    return this->operator()(std::vector<double>(1, val_));
+}
+
+void
+Function::SetParameters(const std::vector<double>& params_){
+    fParams = params_;
+}
+
+std::vector<double>
+Function::GetParameters() const{
+    return fParams;
+}
+
+
+void
+Function::SetParameter(size_t index_, double val_){
+    if (index_ >= fParams.size())
+        throw ParameterError("Tried setting a parameter function does not have!"                             );
+    fParams[index_] = val_;
+}
+
+double
+Function::GetParameter(size_t index_) const{
+    try{
+        return fParams.at(index_);
+    }
+    catch(std::out_of_range&){
+        throw DimensionError("Tried accessing a parameter function does not have!"                             );
+    }
 }
 
--- src/core/function/Function.h
+++ src/core/function/Function.h
@@ -5,11 +5,24 @@
 class Function{
  public:
     Function() : fNDims(0) {}
+    virtual ~Function() {}
+    virtual Function* Clone() const = 0;
+
     virtual double operator()(const std::vector<double>& vals_) const = 0;
+
+    double Call1D(double val) const;
     void SetNDims(unsigned);
     unsigned GetNDims() const;
 
+    // Make functions tunable with a common interface
+    void SetParameters(const std::vector<double>& params_);
+    void SetParameter(size_t index_, double val_);
+
+    std::vector<double> GetParameters()  const;
+    double GetParameter(size_t index_)   const;
+
  protected:
     unsigned fNDims;
+    std::vector<double> fParams;
 };
 #endif
