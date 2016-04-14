73085d714bb876291f707695844d98abb81ef910
Jack Dunger <jack.dunger@physics.ox.ax.uk>
2016-04-14 13:02:12 +0100

    fix typo in FitComponent.hpp

--- src/core/FitComponent.hpp
+++ src/core/FitComponent.hpp
@@ -7,7 +7,7 @@ FitComponent::AddContainerOfParameters(Container& cntr_,
                                        const std::string& sharedName_){
     std::stringstream ss;
     for(size_t i = 0; i < cntr_.size(); i++){
-        ss << sharedName_ << "" << i;
+        ss << sharedName_ << " " << i;
         AddAsParameter(new ContainerParameter<Container>(cntr_, i)
                        , ss.str());
         ss.str("");
