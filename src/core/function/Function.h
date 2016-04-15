9c6321995de377f0faf8dfcc47f6ce7ceb41e1b4
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-04-15 18:54:23 +0100

    make function an iterface

--- src/core/function/Function.h
+++ src/core/function/Function.h
@@ -5,16 +5,11 @@
 
 class Function : public FitComponent{
  public:
-    Function() : fNDims(0) {}
+    Function() {}
     virtual ~Function() {}
     virtual Function* Clone() const = 0;
 
     virtual double operator()(const std::vector<double>& vals_) const = 0;
-    unsigned GetNDims() const;    
- protected:
-    void SetNDims(unsigned nDims_);
-
- private:
-    unsigned fNDims;
+    virtual int GetNDims() const = 0;
 };
 #endif
