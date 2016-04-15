dee2e687da6bb1508555c95e005d6054d716555f
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-04-15 19:28:10 +0100

    bug fix & move functions and analytic pdf over to new parameter system

--- src/core/IFitComponent.h
+++ src/core/IFitComponent.h
@@ -1,14 +1,14 @@
 #ifndef __OXSX_FIT_COMPONENT__
-#ifndef __OXSX_FIT_COMPONENT__
+#define __OXSX_FIT_COMPONENT__
 #include <vector>
 #include <string>
 
 class FitComponent{
  public:
-    virtual std::vector<std::string> GetNames() const = 0;
+    virtual void   MakeFittable() = 0;
+    virtual std::vector<std::string> GetParameterNames() const = 0;
     virtual std::vector<double> GetParameters() const = 0;
     virtual size_t GetParameterCount() const = 0;
-    virtual void   SetParameters(const std::vector<double>&) = 0
-    virtual void   MakeFittable() = 0;
+    virtual void   SetParameters(const std::vector<double>&) = 0;
 };
 #endif
