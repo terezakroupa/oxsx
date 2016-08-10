ae6fe87f7553094d99a3c6a17542e2832c23adf0
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-08-10 13:26:25 +0100

    combs

--- src/core/Combinations.hpp
+++ src/core/Combinations.hpp
@@ -1,7 +1,8 @@
 #ifndef __OXSX_COMBINATIONS__
 #define __OXSX_COMBINATIONS__
-#include <vector>
 #include <Exceptions.h>
+#include <set>
+#include <vector>
 
 template<typename T>
 std::vector<T>
@@ -13,6 +14,13 @@ SequentialElements(T init_, T nElements_){
 	returnVec.push_back(i);
   }
   return returnVec;
+}
+
+template<typename T>
+bool 
+ContainsDuplicates(const std::vector<T>& vals_){
+    std::set<T> s(vals_.begin(), vals_.end());
+    return s.size() != vals_.size();
 }
 
 template<typename T>
@@ -49,6 +57,21 @@ FixedLengthCombinations(const std::vector<T>& vals_, int nElements_){
 
 template<typename T>
 std::vector<std::vector<T> >
+FixedLengthCombinationsNoDuplicates(const std::vector<T>& vals_, int nElements_){
+    std::vector<std::vector<size_t> > retVal;
+    std::vector<std::vector<size_t> > withRepeats = FixedLengthCombinations<T>(vals_, nElements_);
+    for(size_t i = 0; i < withRepeats.size(); i++){
+        std::vector<size_t> combs = withRepeats.at(i);
+        if(!ContainsDuplicates(combs))
+            retVal.push_back(std::vector<T>(combs.begin(), combs.end()));
+    }
+    return retVal;
+}
+
+
+
+template<typename T>
+std::vector<std::vector<T> >
 AllCombinations(const std::vector<T>& vals_){
   typedef std::vector<std::vector<T> > TVecVec;
   typedef std::vector<T> TVec;
@@ -59,6 +82,19 @@ AllCombinations(const std::vector<T>& vals_){
     allCombs.insert(allCombs.end(), iLengthCombs.begin(), iLengthCombs.end());
   }
   return allCombs;
+}
+
+template<typename T>
+std::vector<std::vector<T> >
+AllCombinationsNoDuplicates(const std::vector<T>& vals_){
+    std::vector<std::vector<T> > retVal;
+    std::vector<std::vector<T> > withRepeats = AllCombinations<T>(vals_);    
+    for(size_t i = 0; i < withRepeats.size(); i++){
+        std::vector<T> combs = withRepeats.at(i);
+        if(!ContainsDuplicates(combs))
+            retVal.push_back(std::vector<T>(combs.begin(), combs.end()));
+    }
+    return retVal;
 }
 
 #endif
