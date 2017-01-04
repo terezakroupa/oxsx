49d5df1bc07e2c53170febeffcb6ff92618d969c
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2017-01-04 01:01:57 +0000

    dist filler can fill a cut log

--- src/dist/DistFiller.h
+++ src/dist/DistFiller.h
@@ -2,14 +2,17 @@
 #define __OXSX_DIST_FILLER__
 #include <CutCollection.h>
 #include <EventSystematicManager.h>
+
 class DataSet;
 class BinnedED;
-
+class CutLog;
 class DistFiller{
  public:
   // default is to take all the events
-  static void FillDist(BinnedED&, const DataSet&, 
-                       const CutCollection& cuts_ = CutCollection(), 
+  static void FillDist(BinnedED&, const DataSet&, const CutCollection& cuts_ = CutCollection(), 
+                       EventSystematicManager sysMan_ = EventSystematicManager(), int nEv_ = -1);
+
+  static void FillDist(BinnedED&, const DataSet&, const CutCollection& cuts_, CutLog& log_,
                        EventSystematicManager sysMan_ = EventSystematicManager(), int nEv_ = -1);
 };
 
