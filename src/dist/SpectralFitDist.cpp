f843fc0c85690b94ab852398fef2a1d907af099d
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2017-02-01 16:56:31 +0000

    update spectral fit dist for new fit component interface

--- src/dist/SpectralFitDist.cpp
+++ src/dist/SpectralFitDist.cpp
@@ -2,21 +2,25 @@
 #include <Exceptions.h>
 #include <sstream>
 
-std::vector<std::string> 
-SpectralFitDist::GetParameterNames() const{
-    std::vector<std::string> names;
-    names.reserve(fHistogram.GetNBins());
-
+void
+SpectralFitDist::SetParameterBaseName(const std::string& base_){
+    fParamNames.clear();
+    fParamNames.reserve(fHistogram.GetNBins());
+    
     std::stringstream ss;
     for(size_t i = 0; i < fHistogram.GetNBins(); i++){
-        ss << "Spectral fit bin " << i;
-        names.push_back(ss.str());
+        ss << base_ << i;
+        fParamNames.push_back(ss.str());
         ss.str("");
-    }
-    return names;
+    }    
 }
 
-std::vector<double> 
+std::vector<std::string> 
+SpectralFitDist::GetParameterNames() const{
+    return fParamNames;
+}
+
+std::vector<double>
 SpectralFitDist::GetParameters() const{
     return fHistogram.GetBinContents();
 }
@@ -35,5 +39,29 @@ SpectralFitDist::SetParameters(const std::vector<double>& params_){
         throw ParameterCountError("SpectralFitDist", GetNBins(), 
                                   params_.size(), "is it fittable?");
     }
+}
+
+void
+SpectralFitDist::SetParameterNames(const std::vector<std::string>& names_){
+    if(names_.size() != fHistogram.GetNBins())
+        throw DimensionError("SpectralFitDist::SetParameterNames", fHistogram.GetNBins(), names_.size(), 
+                             "Need one name for each bin, use SetParameterBaseName('myname'); for bins called myname1 myname2 etc.");
+    fParamNames = names_;
+}
+
+double
+SpectralFitDist::GetParameter(const std::string& name_) const{
+    std::vector<std::string>::const_iterator it = std::find(fParamNames.begin(), fParamNames.end(), name_);
+    if(it == fParamNames.end())
+        throw NotFoundError(Formatter() << "SpectralFitDist::No parameter called " << name_);
+    return fHistogram.GetBinContent(it - fParamNames.begin());
+}
+
+void
+SpectralFitDist::SetParameter(const std::string& name_, double val_){
+    std::vector<std::string>::iterator it = std::find(fParamNames.begin(), fParamNames.end(), name_);
+    if(it == fParamNames.end())
+        throw NotFoundError(Formatter() << "SpectralFitDist::No parameter called " << name_);
+    return fHistogram.SetBinContent(it - fParamNames.begin(), val_);
 }
 
--- src/dist/SpectralFitDist.h
+++ src/dist/SpectralFitDist.h
@@ -6,7 +6,7 @@
 
 class SpectralFitDist : public BinnedED, public FitComponent{
  public:
-    SpectralFitDist(const AxisCollection& axes_) : BinnedED(axes_) {}
+    SpectralFitDist(const AxisCollection& axes_) : BinnedED(axes_) {SetParameterBaseName("Bin_");}
 
     // Make this fittable with each bin content adjustable
     void MakeFittable() {}
@@ -14,5 +14,14 @@ class SpectralFitDist : public BinnedED, public FitComponent{
     std::vector<double> GetParameters() const;
     size_t GetParameterCount() const;
     void   SetParameters(const std::vector<double>&);
+    
+    double GetParameter(const std::string&) const;
+    void   SetParameter(const std::string&, double);
+
+    void   SetParameterNames(const std::vector<std::string>&);
+
+    void   SetParameterBaseName(const std::string&);
+ private:
+    std::vector<std::string> fParamNames;
 };
 #endif
