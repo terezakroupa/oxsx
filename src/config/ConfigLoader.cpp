1794bf4e62189d2fb57d5d2f5a7b73e0f8386ff7
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2017-08-30 14:40:50 +0100

    Add `.ini` parser
    Supports all fundamental (non-pointer) c++ types as well as non-associative stl containers of those types
    
    Example use:
    
    filename: example.ini
    ```
    myint = 1
    mychar = 2
    mystring = hello
    myvec = 1, 2, 3
    mset  = 1., 2., 3.
    ```
    
    filename: client.cpp
    ```
    int main(){
        int myint;
        char mychar;
        std::string mystring;
        std::vector<int> myvec;
        std::set<double> myset;
    
        ConfigLoader::Open("example.ini");
        ConfigLoader::Load("mychar", mychar);
        ConfigLoader::Load("mystring", mystring);
        ConfigLoader::Load("myvec", myvec);
        ConfigLoader::Load("myset", myset);
    
        ConfigLoader::Close();
    
        return 0;
    }
    ```

---
+++ src/config/ConfigLoader.cpp
@@ -0,0 +1,28 @@
+#include <ConfigLoader.hh>
+#include <ContainerTools.hpp>
+#include <Exceptions.h>
+#include <sstream>
+
+INI::Parser* ConfigLoader::fParser = NULL;
+
+void
+ConfigLoader::Load(const std::string& fieldName_,  std::string& loadVal_){
+    loadVal_ = fParser-> top()[fieldName_];
+}
+
+void
+ConfigLoader::Open(const std::string& fileName_){
+    std::ifstream ss;
+    ss.open(fileName_.c_str());
+    if(!ss){        
+        throw IOError("Can't find config file " + fileName_);
+    }
+    delete fParser;
+    fParser = new INI::Parser(ss);
+}
+
+void
+ConfigLoader::Close(){
+    delete fParser;
+    fParser = NULL;
+}
