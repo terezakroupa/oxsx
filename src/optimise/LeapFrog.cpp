4c076540c1d3c9a8a3c994dbe459df91918ef04a
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2017-10-27 00:10:00 +0100

    fix reflections in hmc

--- src/optimise/LeapFrog.cpp
+++ src/optimise/LeapFrog.cpp
@@ -2,7 +2,7 @@
 
 double
 LeapFrog::IntersectionTime(double wall_, double initPos_, double vel_){
-    return (wall_ - initPos_)/vel_;    
+    return (wall_ - initPos_)/vel_;
 }
 
 
@@ -15,15 +15,16 @@ LeapFrog::FirstIntersectionTime(const ParameterDict& initPos_,
                                 ){
     crossTime_ = -1;
     dim_ = "";
-
     for(ParameterDict::const_iterator it = initPos_.begin(); it != initPos_.end(); ++it){
         double vel = momenta_.at(it->first)/masses_.at(it->first);
         double mintime = IntersectionTime(minima_.at(it->first), initPos_.at(it->first), vel);
         double maxtime = IntersectionTime(maxima_.at(it->first), initPos_.at(it->first), vel);
 
-        bool mincrossed  = (mintime > 0 && mintime < epsilon_);
-        bool maxcrossed  = (maxtime > 0 && maxtime < epsilon_);
-
+        // only pick up crosses in the right direction
+        // the other way shouldn't happen, but round off error can make it so
+        bool mincrossed  = (mintime > 0 && mintime < epsilon_ && (it->second > minima_.at(it->first)));
+        bool maxcrossed  = (maxtime > 0 && maxtime < epsilon_ && (it->second < maxima_.at(it->first)));
+        
         if(!mincrossed && !maxcrossed)
             continue;
         
@@ -38,6 +39,8 @@ LeapFrog::FirstIntersectionTime(const ParameterDict& initPos_,
             crossTime_ = time;
             dim_ = it->first;
         }
-
+        
     }
+    // if(dim_ != "")
+    //     std::cout << dim_ << " crossed @ " << crossTime_ << std::endl;
 }
